10aac704e12909973bf631962bf57d6cfa49659f
jbrekke <Jeff.Brekke@qg.com>
2010-01-13 09:13:14 -0600

    Moved over some of my stuff

--- jbrekke.el
+++ jbrekke.el
@@ -47,10 +47,34 @@
   "Command to kill a compilation launched by `mode-compile'" t)
 (global-set-key "\C-ck" 'mode-compile-kill)
 
-;; textmate
-(add-to-list 'load-path (concat dotfiles-dir "/vendor/textmate.el"))
-(require 'textmate)
-(textmate-mode)
+;; My KAZ Section!!!
+(defun kaz-kill-all-buffers ()
+  "Kill all buffers and deletes other windows"
+  (interactive)
+  (mapcar (function kill-buffer) (buffer-list))
+  (delete-other-windows))
+
+(defvar kaz-dictionary-url "http://dictionary.reference.com/search?q=%s"
+   "URL used to lookup words.  `%s' is replaced with the word queried")
+   (defun kaz-lookup-word ()
+   "Look up word at point in the dictionary specified by the 
+ `kaz-dictionary-url' variable"
+   (interactive)
+   (save-excursion
+     (let ((url (format kaz-dictionary-url (car (ispell-get-word nil)))))
+      (browse-url url))))
+(autoload 'ispell-get-word  "ispell" "Get word at point using ispell"   t)
+(iswitchb-mode 1)
+(setq read-buffer-function
+          'iswitchb-read-buffer)
+
+;; VMS Section
+;; Modified by dtkim@calab.kaist.ac.kr
+;; Because some VMS ftp servers convert filenames to lower case
+;; we allow a-z in the filename regexp. I'm not too happy about this.
+(setq ange-ftp-vms-filename-regexp "\\([^ ]+ -> [^ ]+\\|[^ ]+\\)$")
+(setq ange-ftp-vms-host-regexp "^qdss21.qg.com$")
+(setq ange-ftp-vms-host-regexp "^qdss20.qg.com$")
 
 (require 'whitespace)
 
@@ -111,7 +135,7 @@
 
 ;; Font
 ;;(set-default-font "-apple-consolas-medium-r-normal--0-0-0-0-m-0-iso10646-1")
-(set-default-font "-microsoft-Consolas-normal-normal-normal-*-16-*-*-*-m-0-iso10646-1")
+;;(set-default-font "-microsoft-Consolas-normal-normal-normal-*-16-*-*-*-m-0-iso10646-1")
 
 ;; Color Themes
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/color-theme"))
@@ -164,11 +188,23 @@
 ;; Other
 (prefer-coding-system 'utf-8)
 
+;; Maven Section
+;;(require 'compile)
+(setq compile-command "mvn clean install")
+(add-to-list
+  'compilation-error-regexp-alist-alist
+   '(mvn "\\(^/.*\\):\\[\\([0-9]+\\),\\([0-9]+\\)\\]" 1 2 3 2 1))
+(add-to-list 'compilation-error-regexp-alist 'mvn)
+(add-to-list
+  'compilation-error-regexp-alist-alist
+   '(mvn-warning "^\\[WARNING\\] \\(/.*\\):\\[\\([0-9]+\\),\\([0-9]+\\)\\]" 1 2 3 1 1))
+(add-to-list 'compilation-error-regexp-alist 'mvn-warning)
+
+
 ;; save the session on exit
 (desktop-save-mode 1)
 
 ;; Activate theme
 ;; (load-file "~/.emacs.d/vendor/color-theme-twilight.el")
 (load-file "~/.emacs.d/vendor/color-theme-vibrant-ink.el")
-
 (color-theme-vibrant-ink)
