9e666b1d61e39f01466c1efecae6110808eeb04d
jrun <jeremy.burks@gmail.com>
2010-01-17 15:12:58 -0600

    use revive to save window layout

--- jburks.el
+++ jburks.el
@@ -165,3 +165,11 @@
 ;;(color-theme-dark-laptop)
 
 (setq  explicit-shell-file-name "/bin/zsh")
+
+(autoload 'save-current-configuration "revive" "Save status" t)
+(autoload 'resume "revive" "Resume Emacs" t)
+(autoload 'wipe "revive" "Wipe Emacs" t)
+
+(define-key ctl-x-map "S" 'save-current-configuration)
+(define-key ctl-x-map "R" 'resume)
+(define-key ctl-x-map "K" 'wipe)
--- jtburks.el
+++ jtburks.el
@@ -172,3 +172,11 @@
 (color-theme-vibrant-ink)
 
 (setq  explicit-shell-file-name "/bin/zsh")
+
+(autoload 'save-current-configuration "revive" "Save status" t)
+(autoload 'resume "revive" "Resume Emacs" t)
+(autoload 'wipe "revive" "Wipe Emacs" t)
+
+(define-key ctl-x-map "S" 'save-current-configuration)
+(define-key ctl-x-map "R" 'resume)
+(define-key ctl-x-map "K" 'wipe)
