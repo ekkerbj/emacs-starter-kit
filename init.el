6150900bc3333c8aff5bb1c55a81998c0dc87031
jbrekke <Jeff.Brekke@qg.com>
2014-01-09 22:55:40 -0600

    not needed

--- init.el
+++ init.el
@@ -68,9 +68,3 @@
 
 
 (provide 'init)
-;;; init.el ends here
-;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
